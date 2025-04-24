dd2dea0f4bf92a3adf1a8b0144aab11c9130acae
= <isangautam@gmail.com>
2025-04-24 08:56:47 +0545

    eslint rules for production

--- backend/server.js
+++ backend/server.js
@@ -2,26 +2,44 @@ const express = require("express")
 const cors = require("cors")
 const socket = require("socket.io")
 const api = require('./routes/api')
+const path = require("path")
+const next = require("next") 
 
 const app = express()
 require('dotenv').config()
 
-app.use(cors());
-app.use(express.json());
+const dev = process.env.NODE_ENV !== "production"
+const appNext = next({ dev, dir: path.join(__dirname, "../frontend") }) 
+const handle = appNext.getRequestHandler()
+
+app.use(cors())
+app.use(express.json())
 app.use('/api', api)
+
 app.get('/', (req, res) => {
-    return res.status(200).json("Welcome to Metaverse API");
-});
-
-const PORT = process.env.PORT
-require('./db')
-
-const server = app.listen(PORT, ()=>{
-    console.log(`Server is running at http://localhost:${PORT}`)
+    return res.status(200).json("Welcome to Metaverse API")
 })
 
-const io = socket(server, {
-    cors:{
-        origin: [process.env.FRONTEND_URL]
-    }
+appNext.prepare().then(() => {
+    app.all("*", (req, res) => {
+        return handle(req, res)
+    })
+
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
+    })
+
+    const PORT = process.env.PORT
+    require('./db')
+
+    const server = app.listen(PORT, () => {
+        console.log(`Server is running at http://localhost:${PORT}`)
+    })
+
+    const io = socket(server, {
+        cors: {
+            origin: "*"
+        }
+    })
 })
