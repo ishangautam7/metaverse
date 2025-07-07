3e66fab56e08a4ccff1674cae1ce32110471b246
ishan <isangatuam@gmail.com>
2025-07-07 17:01:03 +0545

    websocket url changed

--- frontend/src/app/map/lib/socket.ts
+++ frontend/src/app/map/lib/socket.ts
@@ -1,12 +1,14 @@
 import { io, Socket } from "socket.io-client";
 
-const BACKEND_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://metaverse-kz5d.onrender.com"
-    : typeof window !== "undefined" &&
-      window.location.hostname.startsWith("192.168.")
-    ? "http://192.168.1.5:4000"
-    : "http://localhost:4000";
+// const BACKEND_URL =
+//   process.env.NODE_ENV === "prod"
+//     ? "https://metaverse-lrym.onrender.com"
+//     : typeof window !== "undefined" &&
+//       window.location.hostname.startsWith("192.168.")
+//     ? "http://192.168.1.5:4000"
+//     : "http://localhost:4000";
+
+const BACKEND_URL = "https://metaverse-lrym.onrender.com"
 
 export const socket: Socket = io(BACKEND_URL, {
   transports: ['websocket', 'polling'],
