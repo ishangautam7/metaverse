2cc1af8b94f4ce45b873a99527e210c45d74b964
Ishan Gautam <82705547+ishangautam7@users.noreply.github.com>
2026-02-03 06:18:22 +0000

    resolved generatestaticParams and use client for production

--- frontend/src/app/map/[mapUID]/page.tsx
+++ frontend/src/app/map/[mapUID]/page.tsx
@@ -1,55 +1,11 @@
-"use client"
-
-import { useParams } from "next/navigation"
-import CanvaMap from "../components/CanvaMap/CanvaMap"
-import AuthGuard from "@/utils/AuthGuard"
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { checkmaproute } from "@/utils/Routes"
-import { useRouter } from "next/navigation"
-import { Loading } from "@/components/Loading"
+import CanvaRoomClient from "./CanvaRoomClient"
 
 export async function generateStaticParams() {
     return []
 }
 
 const CanvaRoomPage = () => {
-    const router = useRouter()
-    const params = useParams()
-    const [mapExists, setMapExists] = useState<boolean | null>(null)
-    const mapUIDstr = params.mapUID as string
-    const mapUID = Number(mapUIDstr)
-
-    useEffect(() => {
-        const checkMapExistence = async () => {
-            const response = await axios.post(checkmaproute, { mapUID })
-            if (response.status === 200) {
-                setMapExists(response.data.found)
-            }
-        }
-
-        checkMapExistence()
-    }, [mapExists])
-
-    useEffect(() => {
-        if (mapExists === false) {
-            router.push('/');
-        }
-    }, [mapExists, router]);
-
-    if (mapExists === null) {
-        return <Loading />;
-    }
-    const user = JSON.parse(localStorage.getItem("user") ?? "{}")
-    const username = user.username
-    return (
-        <AuthGuard>
-            <div>
-                <CanvaMap username={username} mapUID={mapUID} width={3500} height={2500} />
-            </div>
-
-        </AuthGuard>
-    )
+    return <CanvaRoomClient />
 }
 
 export default CanvaRoomPage
